aede1afe3746e2756a7cd80dfacb3f81fa28a868
devfabiorm <fabio.evangelion.guitar@gmail.com>
2021-01-09 18:45:53 -0300

    Comparando datas e usando o Humanizer para humizar o resultado usando o NuGet

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -11,7 +12,18 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            DateTime dataFimPagamento = new DateTime(2021, 5, 9);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(60);
+
+            string mensagem = "Expires in " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+
             ContaCorrente conta = new ContaCorrente(847, 84728);
+
+            Console.ReadLine();
         }
     }
 }
