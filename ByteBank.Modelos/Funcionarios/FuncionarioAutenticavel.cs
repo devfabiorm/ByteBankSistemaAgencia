2fc286ff98553387e8c42036d3c86f19263fb6c1
devfabiorm <fabio.evangelion.guitar@gmail.com>
2021-01-09 00:58:15 -0300

    Criando a biblioteca e usando o modificar internal para manter clases restritas a ela

--- ByteBank.Modelos/Funcionarios/FuncionarioAutenticavel.cs
+++ ByteBank.Modelos/Funcionarios/FuncionarioAutenticavel.cs
@@ -8,6 +8,7 @@ namespace ByteBank.Modelos.Funcionarios
 {
     public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
     {
+        private AutenticacaoHelper _autenticacaoHelper = new AutenticacaoHelper();
         public string Senha { get; set; }
         public FuncionarioAutenticavel(double salario, string cpf) : base(salario, cpf)
         {
@@ -16,7 +17,7 @@ namespace ByteBank.Modelos.Funcionarios
 
         public bool Autenticar(string senha)
         {
-            return Senha == senha;
+            return _autenticacaoHelper.CompararSenhas(Senha, senha);
         }
     }
 }
